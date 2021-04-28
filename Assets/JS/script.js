315925f90edee50e2adde40ea9eb7080942c145e
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-04-27 17:23:55 -0700

    pulled data from new API call in js, added date to html using moment, made minor changes to CSS for formatting

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,8 @@
 //Assign variables for selectors
 var searchEl = document.querySelector('#search-bar');
 var searchButton = document.querySelector('#search-button');
+var cardContainerEl = document.querySelector('#card-container')
+var dateEl = document.querySelector('#current-date');
 var cardCityName = document.querySelector('#city-name');
 var weatherIconEl = document.querySelector('#weather-icon');
 var cardCityTemp = document.querySelector('#city-temp');
@@ -8,7 +10,13 @@ var cardCityHumid = document.querySelector('#city-humid');
 var cardCityWind = document.querySelector('#city-wind');
 var cardCityUv = document.querySelector('#city-uv');
 
-//initialize variables to be used globally
+
+
+function displayDate() {
+    var rightNow = moment().format('dddd, MMMM Do');
+    dateEl.textContent = rightNow;
+
+};
 
 //function run when the 'search' button is clicked
 searchButton.addEventListener('click', function(event){
@@ -22,6 +30,9 @@ searchButton.addEventListener('click', function(event){
     if(cityName === "") { //if they click search without inputting anything, alert wil pop up
         alert("Please enter a valid city name.")
     } else {
+        var cityCapitalized = cityName.toLowerCase(); //converts the string to all lower case in case user used all caps
+        cityCapitalized = cityCapitalized.charAt(0).toUpperCase() + cityCapitalized.slice(1); //capitalizes first letter of string
+        cardCityName.textContent = cityCapitalized;
         fetch(url)
             .then(function(response){
                 if(response.status !== 200) { //if they city they searched is not found, alert will pop up
@@ -30,45 +41,68 @@ searchButton.addEventListener('click', function(event){
                 return response.json();
             })
             .then(function(data){
-                console.log(data);
                var cityLat = data.coord.lat;
                var cityLon = data.coord.lon;
 
-               console.log(cityLat);
-               console.log(cityLon);
-
-
-                return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=minutely&appid=22da6aaaf94bcdcc6197f3b16198b09d");
+                //returns a new API call using the latitute and longitude collected from the first, also exludes minutely and hourly data b/c not needed
+                return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly&appid=22da6aaaf94bcdcc6197f3b16198b09d");
             })
             .then(function(data){
                 return data.json();
             })
             .then(function(response){
                 console.log(response);
+                var iconUrl = "http://openweathermap.org/img/wn/" + response.current.weather[0].icon +"@2x.png";
+                weatherIconEl.setAttribute('src', iconUrl);
+                weatherIconEl.setAttribute('class', "visible");
+
+                var cityTemp = response.current.temp;
+                
+                cardCityTemp.textContent = "Temperature: " + calculateDegreesF(cityTemp) + " \u00B0 F";
+
+                cardCityHumid.textContent = "Humidity: " + response.current.humidity + " %";
+                cardCityWind.textContent = "Wind: " + response.current.wind_speed + " MPH";
+                cardCityUv.textContent = "UV Index: " + response.current.uvi;
+
+                var dateEl = document.querySelector("#date0");
+                dateEl.textContent = moment().add(1,'days').format('MM/DD');
+
+                var iconEl = document.querySelector("#icon0");
+                var dailyIconUrl = "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon +"@2x.png";
+                iconEl.setAttribute('src', dailyIconUrl)
+
+                var tempEl = document.querySelector("#temp0");
+                dailyTemp = response.daily[0].temp.day
+                tempEl.textContent = "Temperature: " + calculateDegreesF(dailyTemp) + " \u00B0 F";
+
+                var humidEl = document.querySelector("#humid0");
+                humidEl.textContent = "Humidity: " + response.daily[0].humidity + " %";
+
+
+                
             })
+                                
                 
-                // console.log(cityLat);
-                // console.log(cityLon);
+
                 
-                // cardCityName.textContent = cityName;
+                
 
-                // var iconUrl = "http://openweathermap.org/img/wn/" + data.weather[0].icon +"@2x.png";
-                // weatherIconEl.setAttribute('src', iconUrl);
-                // weatherIconEl.setAttribute('class', "visible");
-
-                // var cityTemp = data.main.temp;
-                // cityTemp = Math.floor((cityTemp - 273.15) * (9/5) + 32);
-                // cardCityTemp.textContent = "Temperature: " + cityTemp + " \u00B0 F";
-
-                // cardCityHumid.textContent = "Humidity: " + data.main.humidity + " %";
-                // cardCityWind.textContent = "Wind: " + data.wind.speed + " MPH";
-                // cardCityUv.textContent = "UV Index: " 
+                 
                             
 
         
     }
 })
 
+function calculateDegreesF(temp) {
+    var tempinF = Math.floor((temp - 273.15) * (9/5) + 32);
+    return tempinF;
+}
 
 
 
+function init () {
+    displayDate();
+}
+
+init();
